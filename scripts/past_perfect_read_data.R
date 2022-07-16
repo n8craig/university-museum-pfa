748d7d05982734adae3de8b5cb9a3b6df247d361
ACN\n8craig <n8craig@gmail.com>
2022-07-16 12:45:34 -0600

    using thumbs

--- scripts/past_perfect_read_data.R
+++ scripts/past_perfect_read_data.R
@@ -1,12 +1,9 @@
 
-
+# Load libraries
 library(readxl)
 library(magrittr)
 
 # Read table data
 df <- read_excel(here::here("tables/museum_catalog_complete_2022_07_14.xlsx")) %>% janitor::clean_names()
 
-# Clean columns
-source(here::here("scripts/past_perfect_clean_fields.R"))
 
-save(df, file=here::here("tables/df.Rda"))
