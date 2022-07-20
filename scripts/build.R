b0d60af2bd0a27f09d4b812989940a19bbd02e2c
ACN\n8craig <n8craig@gmail.com>
2022-07-20 15:42:53 -0600

    added finding guide, created four cols, and streamlined UI

--- scripts/build.R
+++
@@ -1,9 +0,0 @@
-# Read PastPerfect Data
-# IMPORTANT: SET CORRECT EXPORT FILE IN THIS SCRIPT
-source(here::here("scripts/past_perfect_read_data.R"))
-
-# Clean columns
-source(here::here("scripts/past_perfect_clean_fields.R"))
-
-# Save data
-save(df, file=here::here("tables/df.Rda"))
